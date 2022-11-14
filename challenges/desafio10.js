086a0c2b10a120b6bd701c3c155578282671caef
martins <gabrielamspinola@gmail.com>
2022-11-14 20:42:53 -0300

    correção Lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,3 @@
 db.produtos.find({ valoresNutricionais: {
-  $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } }
-} }, { "nome": 1, "_id": 0 });
+  $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 }, },
+} }, { nome: 1, _id: 0 });
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,12 +1,12 @@
 db.produtos
-  .find({ 
+  .find({
     $or: [
       { curtidas: { $eq: 36 } },
-      { vendidos: { $eq: 85 }  }
+      { vendidos: { $eq: 85 }  },
     ],
-  }, { 
-    "nome": 1, 
-    "curtidas": 1, 
-    "vendidos": 1, 
-    "_id": 0 
-  });
+}, { 
+    nome: 1, 
+    curtidas: 1, 
+    vendidos: 1, 
+    _id: 0 
+});
